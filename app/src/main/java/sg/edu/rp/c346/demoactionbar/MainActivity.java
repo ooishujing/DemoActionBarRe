30a54c8df77909d2bf37196242ede8382f99eb18
ooishujing <18016012@myrp.edu.sg>
2021-05-27 11:21:53 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/demoactionbar/MainActivity.java
@@ -0,0 +1,22 @@
+package sg.edu.rp.c346.demoactionbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
